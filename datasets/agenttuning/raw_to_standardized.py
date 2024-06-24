d1d640cbc9a1d21c027567cb365c3c71455e978f
Graham Neubig <neubig@gmail.com>
2024-06-24 14:58:27 +0900

    Update for more structure

--- datasets/agenttuning/raw_to_standardized.py
+++ datasets/agenttuning/raw_to_standardized.py
@@ -3,12 +3,30 @@ import sys
 import re
 
 
-def convert_step(step: dict[str, str]):
+def convert_step(step: dict[str, str]) -> list[dict[str, str]]:
+    system_regex = re.match(
+        r"(You are an assistant.*\n\nNow, my problem is:|Now, I will start a new problem in a new OS. My problem is:)\n\n(.*)",
+        step["content"],
+        re.DOTALL,
+    )
     code_act_regex = re.match(r"Think: (.*)\n\nAct: (.*)", step["content"], re.DOTALL)
     code_obs_regex = re.match(
         r"The output of the OS:\n(.*)", step["content"], re.DOTALL
     )
-    if code_act_regex:
+    if system_regex:
+        return [
+            {
+                "class": "text_observation",
+                "content": system_regex.group(1),
+                "source": "system",
+            },
+            {
+                "class": "text_observation",
+                "content": system_regex.group(2),
+                "source": "user",
+            },
+        ]
+    elif code_act_regex:
         code_extract_regex = re.match(
             r"bash\n\n```bash\n(.*)\n```", code_act_regex.group(2), re.DOTALL
         )
@@ -16,44 +34,56 @@ def convert_step(step: dict[str, str]):
             r"answer\((.*)\)", code_act_regex.group(2), re.DOTALL
         )
         if code_extract_regex:
-            return {
-                "class": "code_action",
-                "language": "bash",
-                "content": code_extract_regex.group(1),
-                "description": code_act_regex.group(1),
-            }
+            return [
+                {
+                    "class": "code_action",
+                    "language": "bash",
+                    "content": code_extract_regex.group(1),
+                    "description": code_act_regex.group(1),
+                }
+            ]
         elif answer_extract_regex:
-            return {
-                "class": "message_action",
-                "content": answer_extract_regex.group(1),
-                "description": code_act_regex.group(1),
-            }
+            return [
+                {
+                    "class": "message_action",
+                    "content": answer_extract_regex.group(1),
+                    "description": code_act_regex.group(1),
+                }
+            ]
         else:
             raise ValueError(
                 "Could not extract code from code action in"
                 f" {json.dumps(step, indent=2)}"
             )
     elif code_obs_regex:
-        return {
-            "class": "text_observation",
-            "content": code_obs_regex.group(1),
-            "source": "os",
-        }
+        return [
+            {
+                "class": "text_observation",
+                "content": code_obs_regex.group(1),
+                "source": "os",
+            }
+        ]
     else:
-        return {
-            "class": "text_observation",
-            "content": step["content"],
-            "source": "user",
-        }
+        return [
+            {
+                "class": "text_observation",
+                "content": step["content"],
+                "source": "user",
+            }
+        ]
 
 
 for line in sys.stdin:
     raw_data = json.loads(line)
 
+    content = []
+    for step in raw_data["conversations"]:
+        content.extend(convert_step(step))
+
     # Standardize the data
     standardize_data = {
         "id": raw_data["id"],
-        "content": [convert_step(x) for x in raw_data["conversations"]],
+        "content": content,
     }
 
     # Print the standardized data
