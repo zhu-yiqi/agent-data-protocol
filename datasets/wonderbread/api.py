760ad52aa6ba3f65bf4e41e24dc1ff6327d6f070
alexisxy <alexisxy0418@gmail.com>
2024-07-02 22:44:58 -0400

    add wonderbread

---
+++ datasets/wonderbread/api.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+
+def mouseup(element: dict[str, Any], misc: dict[str, Any]) -> None:
+    pass
+
+
+def keystroke(element: dict[str, Any], str: str) -> None:
+    pass
+
+
+def scroll(x: float, y: float) -> None:
+    pass
