690e63d83c678ae49596bf6f27af7d580b3125f2
ronch99 <ziruch99@gmail.com>
2024-07-04 09:43:08 -0400

    added api for llava_plus

---
+++ datasets/llava_plus/api.py
@@ -0,0 +1,19 @@
+def sam(bbox: list[list]) -> None:
+    """Call SAM (Segment Anything Model) to detect objects.
+
+    Args:
+    ----
+        bbox (list[list]): An array of bounding box coordinates.
+
+    """
+    pass
+
+def inpainting(prompt: str) -> None:
+    """Call stable diffusion inpainting model to edit images.
+
+    Args:
+    ----
+        prompt (str): An instruction for image editing.
+
+    """
+    pass
