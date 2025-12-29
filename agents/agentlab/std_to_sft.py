ae0f1b59af5cefdd462697e15ffb51659373dbfe
Yueqi Song <141804823+yueqis@users.noreply.github.com>
2025-12-29 06:27:56 -0500

    Update std_to_sft.py

--- agents/agentlab/std_to_sft.py
+++ agents/agentlab/std_to_sft.py
@@ -12,43 +12,66 @@ assert dataset, "Please set the environment variable MY_DATASET"
 with open("agents/agentlab/system.txt") as f:
     system = f.read()
 with open("agents/agentlab/action_space.txt") as f:
-    action_space = f.read()
+    action_space = f.read().strip()
 with open("agents/agentlab/suffix.txt") as f:
-    suffix = f.read()
+    suffix = f.read().strip()
 
 
-def process_row(line):
+def process_row(line, id_to_openhands_sft):
     std_dataset = [json.loads(line)]
     std_data = std_dataset[0]
     trajectory = Trajectory(**std_data)
     events = trajectory.content
-    output_line = json.loads(main_openhands(line, is_web=True, api_env="browser"))
+    if trajectory.id not in id_to_openhands_sft:
+        print(f"{trajectory.id} not found", file=sys.stderr)
+        return None
+    output_line = id_to_openhands_sft[trajectory.id]
+    # output_line = json.loads(main_openhands(line, is_web=True, api_env="browser"))
     goal = "# Goal\n" + events[0].content + "\n\n"
     past_actions = []
     observation = ""
+    ret = []
     for step in range(len(output_line["conversations"])):
         if step % 2 == 1:
             match = re.search(
-                r"(<function=browser>\n<parameter=code>\n)(.*?)(\n</parameter>\n</function>)",
+                # r"(<function=browser>\n<parameter=code>\n)(.*?)(\n</parameter>\n</function>)",
+                r'^(?P<thought>.*?)<function=browser>\s*<parameter=code>\s*(?P<action>.*?)\s*</parameter>',
                 output_line["conversations"][step]["value"],
                 flags=re.DOTALL,
             )
             if not match:
-                action = ""
+                match = re.search(
+                    r'^(?P<thought>.*?)<function=finish>\s*<parameter=message>\s*(?P<action>.*?)\s*</parameter>',
+                    output_line["conversations"][step]["value"],
+                    flags=re.DOTALL,
+                )
+                if match:
+                    thought = match.group("thought").strip()
+                    message = match.group("action").strip()
+                elif '<function=' not in output_line["conversations"][step]["value"]:
+                    thought = ""
+                    message = output_line["conversations"][step]["value"]
+                else:
+                    raise ValueError(f'no match: {output_line["conversations"][step]["value"]}')
+                action = f'send_msg_to_user(text="{message}")'
+                
             else:
-                thought, action, _ = match.groups()
-                action = json.dumps({"thought": thought, "action": action}).strip()
-                past_actions.append(action)
-                action = {"role": "assistant", "content": action}
-                if observation:
-                    return {
-                        "id": f"{trajectory.id}-{step // 2}",
-                        "conversations": [observation, action],
-                        "system": system,
-                    }
+                thought = match.group("thought").strip()
+                action = match.group("action").strip()
+            action = json.dumps({"thought": thought, "action": action}).strip()
+            past_actions.append(action)
+            action = {"role": "assistant", "content": action}
+            if observation:
+                ret.append({
+                    "id": f"{trajectory.id}-{step // 2}",
+                    "conversations": [observation, action],
+                    "system": system,
+                })
+            else: 
+                raise ValueError(f'no observation: {output_line["conversations"][step]["value"]}')
         else:
             match = re.search(
-                r"(BEGIN accessibility tree ==============)(.*?)(END accessibility tree ==============)",
+                r"(============== BEGIN accessibility tree ==============)(.*?)(============== END accessibility tree ==============)",
                 output_line["conversations"][step]["value"],
                 flags=re.DOTALL,
             )
@@ -56,17 +79,24 @@ def process_row(line):
                 tree = ""
             else:
                 _, tree, _ = match.groups()
-                tree = "# Current page Accessibility Tree\n" + tree + "\n\n"
-            history = "# History of past actions\n" + "\n".join(past_actions) + "\n\n"
+                tree = "# Current page Accessibility Tree\n" + tree.strip() + "\n\n"
+            history = "\n\n\n\n\n# History of past actions\n" + "\n".join(past_actions) + "\n\n"
             observation = {"role": "user", "content": goal + tree + action_space + history + suffix}
-
+    return ret
 
 def main():
+    with open("/home/yueqis/agent-data-protocol/datasets/mind2web/full_sft/full_sft_openhands.jsonl") as f:
+        f = f.readlines()
+    openhands_sft = [json.loads(line) for line in f]
+    id_to_openhands_sft = {line["id"]: line for line in openhands_sft}
+    
+    
     for line in sys.stdin:
-        output_line = process_row(line)
-        if output_line:
-            output_line = json.dumps(output_line)
-            print(output_line)
+        output_lines = process_row(line, id_to_openhands_sft)
+        if output_lines:
+            for output_line in output_lines:
+                output_line = json.dumps(output_line)
+                print(output_line)
 
 
 if __name__ == "__main__":
